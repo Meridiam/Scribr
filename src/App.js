6cf7dca4587db767e1d996fa85d304544baacc15
Jerry Peng <png.jrry@gmail.com>
2018-09-08 22:14:41 -0400

    fixed issue that could arise if stop or start were pressed multiple times in a row

--- src/App.js
+++ src/App.js
@@ -6,76 +6,82 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = {transcript: ""};
+    this.state = {transcript: "", recording: false};
   }
 
   handleRecord() {
-    navigator.mediaDevices.getUserMedia({audio: true, video: false})
-    .then((stream) => {
-      this.mr = new MediaRecorder(stream);
-      this.audioChunks = [];
-      this.mr.ondataavailable = e => {
-        //inside callback function when recording is done
-        console.log("recording...");
-        this.audioChunks.push(e.data);
-        console.log(this.audioChunks);
-
-        let blob = new Blob(this.audioChunks, { 'type': 'audio/ogg; codecs=opus' });
-        // var xhr = new XMLHttpRequest();
-        // xhr.open('POST', 'https://scribr-backend.herokuapp.com/transcribe', true);
-        // xhr.setRequestHeader("Content-Type", "multipart/form-data");
-
-        // xhr.send(blob);
-
-        const data = new FormData();
-        data.append('audio', blob, 'audio.opus');
-
-        console.log(data);
-
-        axios.create ({
-          baseURL: 'http://scribr-backend.herokuapp.com',
-          timeout: 10000}).post(`/transcribe`, data, {
-            headers: {
-            'Content-Type': `multipart/form-data;
-            boundary = ${data._boundary}`,
-          },
-          timeout: 30000,
-        })
-        .then(response => {
-          console.log(response.data);
-          this.setState({transcript: response.data["transcript"]});
-        });
-        
-        let blobURL = window.URL.createObjectURL(blob);
-        var audioPlayer = document.createElement("AUDIO");
-        audioPlayer.src = blobURL;      
-        audioPlayer.setAttribute("id", "player");
-        audioPlayer.setAttribute("controls", "controls");
-        document.body.appendChild(audioPlayer);
-
-
-       // var myBlob = new Blob(["This is my blob content"], {type : "text/plain"});
-        //USE AXIOS TO SEND POST REQUEST TO SERVER WITH BLOB AS PART OF A FORM (LOOK IT UP)
-     //   var fd = new FormData();
-
-
-        //fd.append('audio', blob, 'audio.opus');
-
-
-        // fetch('https://scribr-backend.herokuapp.com/transcribe', {
-        //   method: 'post',
-        //   data: fd
-        // }).then(function() {
-        //   console.log('done');
-        // }).catch(err => alert(err))
-      }
-
-    this.mr.start();
-    });
+    if (!this.state.recording) {
+      navigator.mediaDevices.getUserMedia({audio: true, video: false})
+      .then((stream) => {
+        this.mr = new MediaRecorder(stream);
+        this.audioChunks = [];
+        this.mr.ondataavailable = e => {
+          //inside callback function when recording is done
+          console.log("recording...");
+          this.audioChunks.push(e.data);
+          console.log(this.audioChunks);
+  
+          let blob = new Blob(this.audioChunks, { 'type': 'audio/ogg; codecs=opus' });
+          // var xhr = new XMLHttpRequest();
+          // xhr.open('POST', 'https://scribr-backend.herokuapp.com/transcribe', true);
+          // xhr.setRequestHeader("Content-Type", "multipart/form-data");
+  
+          // xhr.send(blob);
+  
+          const data = new FormData();
+          data.append('audio', blob, 'audio.opus');
+  
+          console.log(data);
+  
+          axios.create ({
+            baseURL: 'http://scribr-backend.herokuapp.com',
+            timeout: 10000}).post(`/transcribe`, data, {
+              headers: {
+              'Content-Type': `multipart/form-data;
+              boundary = ${data._boundary}`,
+            },
+            timeout: 30000,
+          })
+          .then(response => {
+            console.log(response.data);
+            this.setState({transcript: response.data["transcript"]});
+          });
+          
+          let blobURL = window.URL.createObjectURL(blob);
+          var audioPlayer = document.createElement("AUDIO");
+          audioPlayer.src = blobURL;      
+          audioPlayer.setAttribute("id", "player");
+          audioPlayer.setAttribute("controls", "controls");
+          document.body.appendChild(audioPlayer);
+  
+  
+         // var myBlob = new Blob(["This is my blob content"], {type : "text/plain"});
+          //USE AXIOS TO SEND POST REQUEST TO SERVER WITH BLOB AS PART OF A FORM (LOOK IT UP)
+       //   var fd = new FormData();
+  
+  
+          //fd.append('audio', blob, 'audio.opus');
+  
+  
+          // fetch('https://scribr-backend.herokuapp.com/transcribe', {
+          //   method: 'post',
+          //   data: fd
+          // }).then(function() {
+          //   console.log('done');
+          // }).catch(err => alert(err))
+        }
+  
+      this.mr.start();
+      this.setState({transcript: this.state.transcript, recording: true})
+      });
+    }
   }
 
   handleStop() {
-    this.mr.stop();
+    if (this.state.recording) {
+      this.mr.stop();
+      this.setState({transcript: this.state.transcript, recording: false})
+    }
   }
 
   render() {
